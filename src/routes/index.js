a04f2a5d01060b6459d8512ed068a18bc3d6211d
Timothy Goncharov <sedmoy.mm@mail.ru>
2017-07-19 00:35:30 +0300

    Created structure project, changed config of project.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
-import Home from './Home'
+import Home from './home'
+import Main from './main'
 
 export const createRoutes = () => (
-  <Route path="/" component={Home}>
+  <Route path="/" component={Main}>
+    <Route path="home" component={Home} />
   </Route>
 )
 
