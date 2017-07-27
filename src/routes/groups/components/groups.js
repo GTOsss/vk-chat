edfd63abf7ce48af0080178aba6ff7cf19ce0633
Timothy Goncharov <sedmoy.mm@mail.ru>
2017-07-27 20:11:15 +0300

    View edited.

--- src/routes/groups/components/groups.js
+++ src/routes/groups/components/groups.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import ListGroups from '../../../components/list-groups'
-import cx from 'classnames'
 
 import style from './groups.scss'
 
 const Groups = ({groups, onClickItemListHandler}) => (
-  <div className='container-fluid'>
-    <div className='row'>
-      <div className={cx('col-md-7', style['ul-screen'])}>
-        <ListGroups groups={groups} onClickItemListHandler={onClickItemListHandler} />
+  <div className={style['wrap-scroll']}>
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className={'col-md-7'}>
+          <div className={style['ul-screen']}>
+            <ListGroups groups={groups} onClickItemListHandler={onClickItemListHandler}/>
+          </div>
+        </div>
+        <div className={'col-md-5'}>
+          <div className={style['search-screen']}>
+            search params
+          </div>
+        </div>
       </div>
     </div>
   </div>
 );
 
 export default Groups
-
