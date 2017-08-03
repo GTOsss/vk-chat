105d7b9a324089ed5fe74444810bea32b5181674
Timothy Goncharov <sedmoy.mm@mail.ru>
2017-08-03 18:45:55 +0300

    View and components fix.

--- src/components/list-users/index.js
+++ src/components/list-users/index.js
@@ -6,16 +6,22 @@ import style from './list-users.scss'
 const addSpaceNumber = (number) => number.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
 
 const createList = (users) => users.map((el, i) => (
-  <a href={`https://www.vk.com/id${el.id}`}  target='_blank'>
-    <UserItem key={i} className={style['user-item']}
+  <a href={`https://www.vk.com/id${el.id}`} key={i} target='_blank'>
+    <UserItem className={style['user-item']}
             name={`${el.first_name} ${el.last_name}`}
             followersCount={`${addSpaceNumber(el.followers_count)} подписчиков`}
             srcImg100={el.photo_100} />
   </a>
 ));
 
-const ListUsers = ({users}) => (
+const ListUsers = ({users, headerText}) => (
   <ul className={style['ul-users']}>
+    {headerText ?
+      <div style={{display: 'flex', flexDirection: 'row', marginBottom: '4px'}}>
+        <div className={style['ul-header-left-right']}/>
+        <div className={style['ul-header']}>{headerText}</div>
+        <div className={style['ul-header-left-right']}/>
+      </div> : ''}
     {users ? createList(users) : ''}
   </ul>
 );
--- src/routes/groups/search-results/components/list-users.js
+++ src/routes/groups/search-results/components/list-users.js
@@ -6,12 +6,8 @@ import style from './search-results.scss'
 
 const listUsers = ({users}) => (
   <div className={style['ul-screen']}>
-    <div style={{display: 'flex', flexDirection: 'row', marginBottom: '4px'}}>
-      <div className={style['ul-header-left-right']}/>
-      <div className={style['ul-header']}>{`Найдено ${users.length}`}</div>
-      <div className={style['ul-header-left-right']}/>
-    </div>
-    { (users && users.length) ? <ListUsersComponent users={users}/> : '' }
+    { (users && users.length) ? <ListUsersComponent users={users}
+                                                    headerText={`Найдено ${users.length}`}/> : '' }
   </div>
 );
 
