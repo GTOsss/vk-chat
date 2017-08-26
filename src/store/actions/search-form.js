e3a8b16d38f5bfa229a44638dd7de008cfac978e
Timothy Goncharov <sedmoy.mm@mail.ru>
2017-08-26 12:17:23 +0300

    Fix error with cities input. Set timeout on request.

--- src/store/actions/search-form.js
+++ src/store/actions/search-form.js
@@ -1,9 +1,10 @@
-import {testResponse} from '../../services/vk-service'
+import {testResponse, vkApiTimeout} from '../../services/vk-service'
 import {formValueSelector} from 'redux-form'
 import {
   UPDATE_COUNTRY_REQUEST,
   UPDATE_COUNTRY_SUCCESS,
-  UPDATE_COUNTRY_FAIL
+  UPDATE_COUNTRY_FAIL,
+  TOGGLE_LOADING
 } from '../constans/index'
 
 export const updateCountries = () => {
@@ -42,32 +43,38 @@ export const updateCountries = () => {
 
 export const updateCities = (input, callback) => {
   return (dispatch, getState) => {
+    console.log(getState().loading.loadingObj.citiesTimerId);
+    clearTimeout(getState().loading.loadingObj.citiesTimerId);
+    dispatch({
+      type: TOGGLE_LOADING,
+      loadingObj: {citiesTimerId: setTimeout(() => dispatch(loadCities(input, callback)), 300)}
+    });
+  }
+};
+
+const loadCities = (input, callback) => {
+  return async (dispatch, getState) => {
     const selector = formValueSelector('searchParams');
     const values = selector(getState(), 'country', 'city');
     if(!(values && values.country && input)) {
       callback(null, {options: [], complete: true});
       return
     }
-    VK.api('database.getCities', {
+
+    const {response: {items}} = await vkApiTimeout('database.getCities', {
       'country_id': values.country,
       'q': input,
       'need_all': 1,
       'offset': 0,
       'count': 999,
       'version': 5.67
-    }, (resp) => {
-      try{
-        testResponse(resp);
-        const {response: {items}} = resp;
-        const cities = items.map((el) => ({value: el.id, label: el.title}));
-        cities.sort((a, b) => {
-          if(a.value > b.value) return 1;
-          if(a.value < b.value) return -1;
-        });
-        callback(null, {options: cities, complete: true});
-      }
-      catch(e) {
-      }
+    }, 200);
+
+    const cities = items.map((el) => ({value: el.id, label: el.title}));
+    cities.sort((a, b) => {
+      if(a.value > b.value) return 1;
+      if(a.value < b.value) return -1;
     });
+    callback(null, {options: cities, complete: true});
   }
 };
