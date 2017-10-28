472ef322aa8548dcb99dbc6d94d7718927d7075f
Timothy Goncharov <sedmoy.mm@mail.ru>
2017-10-28 19:41:43 +0300

    Refactored loader-component.

--- src/components/loader/index.js
+++ src/components/loader/index.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import LoaderIcon from 'react-icons/lib/fa/refresh'
+import React from 'react';
+import LoaderIcon from 'react-icons/lib/fa/refresh';
+import PropTypes from 'prop-types';
 
-import style from './loader.scss'
+import style from './loader.scss';
 
-const Loader = ({size = 56, mini}) => (
-  <div className={style['wrap-loader']}
-       style={mini ? {lineHeight: '80px', marginTop: '-15px'} : {}}>
-    <LoaderIcon className={style['icon']} color='#507299' size={mini ? 24 : size} />
+const Loader = ({ size, mini }) => (
+  <div
+    className={style['wrap-loader']}
+    style={mini ? { lineHeight: '80px', marginTop: '-15px' } : {}}
+  >
+    <LoaderIcon className={style.icon} color="#507299" size={mini ? 24 : size} />
   </div>
 );
 
-export default Loader
+Loader.propTypes = {
+  size: PropTypes.number,
+  mini: PropTypes.bool,
+};
+
+Loader.defaultProps = {
+  size: 56,
+  mini: false,
+};
+
+export default Loader;
