dab00fd47a5aff4d83ce7f3349921ead6ea39fb9
Timothy Goncharov <sedmoy.mm@mail.ru>
2017-10-28 13:02:27 +0300

    Refactored progress-bar-component.

--- src/components/progress-bar/index.js
+++ src/components/progress-bar/index.js
@@ -1,27 +1,37 @@
-import React from 'react'
-import cx from 'classnames'
-import PropTypes from 'prop-types'
+import React from 'react';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
 
-import style from './progress-bar.scss'
+import style from './progress-bar.scss';
 
 const styleLine = {
   height: '20px',
   transition: '500ms',
 };
 
-const ProgressBar = ({className, progress, lineColor = '#5e81a8', children}) => (
+const ProgressBar = ({ className, progress, lineColor, children }) => (
   <div>
-    {children ? <div className={style['text-label']}>{children}</div> : ''}
+    {children ?
+      <div className={style['text-label']}>
+        {children}
+      </div> : ''}
     <div className={cx(className, style['progress-bar-box'])}>
-      <div style={{backgroundColor: lineColor, width: `${progress}%`, ...styleLine}} />
+      <div style={{ backgroundColor: lineColor, width: `${progress}%`, ...styleLine }} />
     </div>
   </div>
 );
 
 ProgressBar.propTypes = {
+  children: PropTypes.any, // eslint-disable-line
   className: PropTypes.string,
-  progress: PropTypes.number,
-  lineColor: PropTypes.string
+  progress: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  lineColor: PropTypes.string,
 };
 
-export default ProgressBar
+ProgressBar.defaultProps = {
+  className: '',
+  progress: 0,
+  lineColor: '#5e81a8',
+};
+
+export default ProgressBar;
