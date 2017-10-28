7d9fa7734af13f1104b31dadd265c47ff155c9ba
Timothy Goncharov <sedmoy.mm@mail.ru>
2017-10-28 19:39:54 +0300

    Refactored nav-bar-component.

--- src/components/nav-bar/index.js
+++ src/components/nav-bar/index.js
@@ -1,26 +1,29 @@
-import React from 'react'
-import cx from 'classnames'
+import React from 'react';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
 
-import style from './nav-bar.scss'
+import style from './nav-bar.scss';
 
 const navbarStyle = cx('navbar navbar-inverse navbar-toggleable-md ' +
   'navbar-light bg-faded', style.navbar);
 
-const NavBar = ({title, children}) => (
+const NavBar = ({ title, children }) => (
   <div>
     <nav className={navbarStyle}>
-      <button className='navbar-toggler navbar-toggler-right'
-              type='button'
-              data-TOGGLE_LOADING='collapse'
-              data-target='#navbarTogglerDemo02'
-              aria-controls='navbarTogglerDemo02'
-              aria-expanded='false'
-              aria-label='TOGGLE_LOADING navigation'>
-        <span className='navbar-toggler-icon' />
+      <button
+        className="navbar-toggler navbar-toggler-right"
+        type="button"
+        data-TOGGLE_LOADING="collapse"
+        data-target="#navbarTogglerDemo02"
+        aria-controls="navbarTogglerDemo02"
+        aria-expanded="false"
+        aria-label="TOGGLE_LOADING navigation"
+      >
+        <span className="navbar-toggler-icon" />
       </button>
-      {title ? title : ''}
-      <div className='collapse navbar-collapse' id='navbarTogglerDemo02'>
-        <ul className='navbar-nav mr-auto mt-2 mt-md-0'>
+      {title || ''}
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul className="navbar-nav mr-auto mt-2 mt-md-0">
           {children}
         </ul>
       </div>
@@ -28,4 +31,13 @@ const NavBar = ({title, children}) => (
   </div>
 );
 
-export default NavBar
+NavBar.propTypes = {
+  children: PropTypes.any, // eslint-disable-line
+  title: PropTypes.string,
+};
+
+NavBar.defaultProps = {
+  title: '',
+};
+
+export default NavBar;
--- src/components/nav-bar/nav-link/index.js
+++ src/components/nav-bar/nav-link/index.js
@@ -1,20 +1,35 @@
-import React from 'react'
-import {Link} from 'react-router'
-import cx from 'classnames'
+import React from 'react';
+import { Link } from 'react-router';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
 
-import style from './nav-link.scss'
+import style from './nav-link.scss';
 
-const navItemStyles = cx('nav-item', 'nav-link',style['nav-item']);
+const navItemStyles = cx('nav-item', 'nav-link', style['nav-item']);
 const navItemActiveStyle = cx('nav-item active', style['nav-item-active']);
 
-const NavLink = ({to, children, onClick, noActive}) => (
+const NavLink = ({ to, children, onClick, noActive }) => (
   <li>
-    <Link className={navItemStyles}
-          activeClassName={noActive ? '' : navItemActiveStyle}
-          to={to}>
+    <Link
+      className={navItemStyles}
+      activeClassName={noActive ? '' : navItemActiveStyle}
+      to={to}
+    >
       {children}
     </Link>
   </li>
 );
 
-export default NavLink
+NavLink.propTypes = {
+  children: PropTypes.any, // eslint-disable-line
+  to: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  noActive: PropTypes.bool,
+};
+
+NavLink.defaultProps = {
+  onClick: null,
+  noActive: false,
+};
+
+export default NavLink;
