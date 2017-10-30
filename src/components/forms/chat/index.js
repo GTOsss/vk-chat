f02b698515cbd70632dd420928888a2c9941d45e
Timothy Goncharov <sedmoy.mm@mail.ru>
2017-10-30 13:15:40 +0300

    Refactored chat-form.

--- src/components/forms/chat/index.js
+++ src/components/forms/chat/index.js
@@ -1,22 +1,35 @@
-import React from 'react'
-import {Field, reduxForm} from 'redux-form'
-import TextArea from '../inputs/text-area'
-import SendIcon from 'react-icons/lib/md/send'
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import SendIcon from 'react-icons/lib/md/send';
+import PropTypes from 'prop-types';
+import TextArea from '../inputs/text-area';
 
-import style from './chat.scss'
+import style from './chat.scss';
 
-const Chat = ({handleSubmit}) => (
+const Chat = ({ handleSubmit }) => (
   <form onSubmit={handleSubmit}>
-    <div className={style['wrap']}>
-      <Field name='message'
-             component={(props) => <TextArea {...props}
-                                             placeholder='Напишите сообщение...'
-                                             cssWrap={{width: '80%'}}/>}/>
+    <div className={style.wrap}>
+      <Field
+        name="message"
+        component={props => (<TextArea
+          {...props}
+          placeholder="Напишите сообщение..."
+          cssWrap={{ width: '80%' }}
+        />)}
+      />
       <div className={style['send-icon']} onClick={handleSubmit}>
-        <SendIcon size={28} color={'#A6ABB6'}/>
+        <SendIcon size={28} color={'#A6ABB6'} />
       </div>
     </div>
   </form>
 );
 
-export default reduxForm({form: 'chat'})(Chat)
+Chat.propTypes = {
+  handleSubmit: PropTypes.func,
+};
+
+Chat.defaultProps = {
+  handleSubmit: null,
+};
+
+export default reduxForm({ form: 'chat' })(Chat);
